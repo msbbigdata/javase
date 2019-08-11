bc3ee42fabe6700fe924c7788500a7c8d6357033
msbbigdata <1899941519@qq.com>
2019-08-11 17:12:18 +0800

    upload

--- code/2019-08-10/OperatorDemo.java
+++ code/2019-08-10/OperatorDemo.java
@@ -90,5 +90,41 @@ public class OperatorDemo{
 		System.out.println(3>2?3:2);
 		System.out.println(false?false:true?false:true);
 		
+		
+		/*
+		
+		基本数据类型之间的转换
+			自动转换(隐形转换)：
+			强制转换
+			
+			注意:
+				1、在进行算术运算操作的时候，必须要求数据类型一致，否则无法操作
+				2、在运算过程中，如果两个值得类型不一致，会自动将小的类型转换为大的类型
+				3、在运算过程中，可以手动强制转换，将大的类型转换为小的类型
+					实现方式，(datatype)
+				4、强制转换，会发生精度损失，结果可能不准确
+		*/
+		byte bb = 10;
+		int aa = 200;
+		byte cc;
+		cc = (byte)(aa+bb);
+		System.out.println(cc);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
